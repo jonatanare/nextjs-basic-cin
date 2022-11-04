15e1188d1982aab1cd3d30008edc4d33bc687a86
jonatanare <jonatanare.dev@gmail.com>
2022-11-04 13:06:37 -0500

    project basic next js

---
+++ pages/nosotros.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function nosotros() {
+  return (
+    <Layout>
+        <div>nosotros</div>
+    </Layout>
+  )
+}
